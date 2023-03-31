ee5df0ba0fb7b038af820bde283dd8403a7ea6ac
mfarhadattari <mfarhad.attari@gmail.com>
2023-04-01 00:06:48 +0600

    header component nav bar  design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
         <a href="#home">Programming Cafe</a>
       </div>
       <nav className="flex gap-5 items-center font-semibold text-xl">
-        <a href="#home">Home</a>
-        <a href="#blog">Blog</a>
-        <a href="#contract">Contract</a>
-        <a href="#dashboard" className="bg-slate-500 rounded-full px-3 py-3 h-[50px] w-[50px] text-center"><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></a>
+        <a href="#home" className="hover:bg-pink-600 px-3 py-2 rounded-xl">Home</a>
+        <a href="#blog" className="hover:bg-pink-600 px-3 py-2 rounded-xl">Blog</a>
+        <a href="#contract" className="hover:bg-pink-600 px-3 py-2 rounded-xl">Contract</a>
+        <a href="#dashboard" className="bg-slate-500 hover:bg-pink-600 rounded-full px-3 py-3 h-[50px] w-[50px] text-center"><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></a>
       </nav>
     </div>
   );
