56574923fbb9f4d2c455779def5ba82985757785
mfarhadattari <mfarhad.attari@gmail.com>
2023-04-01 22:47:25 +0600

    add border on bookmark component

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Bookmark = ({post}) => {
     // console.log(post)
     return (
-        <div className='p-5 bg-white my-4 rounded-lg'>
+        <div className='p-5 bg-white my-4 rounded-lg border'>
             <h1 className='font-semibold text-lg'>{post.postTitle}</h1>
         </div>
     );
