12692943a1e2a58e2825214b5da130f404953705
mfarhadattari <mfarhad.attari@gmail.com>
2023-04-01 17:21:48 +0600

    footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='bg-slate-900 w-full mt-10'>
+            <div className='container mx-auto p-10 text-xl text-white font-medium'>
+                <p className='text-center'>&copy; All Right Reserved | Programming Cafe</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
